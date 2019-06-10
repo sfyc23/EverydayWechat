17178697c5a59d1ada813480dbc6b1c090dd94be
lkg <627861816@qq.com>
2019-06-10 19:07:10 +0800

    review code 代码规范

--- run.py
+++ run.py
@@ -2,23 +2,21 @@ from GFWeather import GFWeather
 
 
 def run():
-    '''
+    """
     主程序入口
     :return:
-    '''
+    """
     GFWeather().run()
 
 
 def test_run():
-    '''
+    """
     运行前的测试
     :return:
-    '''
-    GFWeather().start_today_info(is_test=True)
+    """
+    GFWeather().start_today_info()
+
 
 if __name__ == '__main__':
     # test_run()
     run()
-
-
-
