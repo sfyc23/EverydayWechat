ac9a7b2a150dc45fdb294c402abe8e85d1335d63
sfyc23 <sfyc23@gmail.com>
2019-06-29 11:39:15 +0800

    优化星座运势，添加万年历，添加「在一起」文案

--- everyday_wechat/utils/data_collection.py
+++ everyday_wechat/utils/data_collection.py
@@ -1,21 +1,30 @@
 # coding=utf-8
 
 import importlib
+import re
 from datetime import datetime
 
 from everyday_wechat.control.weather.rtweather import get_today_weather
+from everyday_wechat.control.weather.sojson import get_sojson_weather
 from everyday_wechat.utils.common import (
-    get_yaml
+    get_yaml,
+    WEEK_DICT,
+    get_constellation_name,
 )
-from everyday_wechat.control.horoscope.spider_script import get_constellation, get_xzw_data_list
+from everyday_wechat.utils import config
+
+from everyday_wechat.control.horoscope.xzw_horescope import get_today_horoscope
+from everyday_wechat.control.calendar.sojson_calendar import get_sojson_calendar
 
 DICTUM_NAME_DICT = {1: 'wufazhuce', 2: 'acib', 3: 'lovelive', 4: 'hitokoto', 5: 'rtjokes', 6: 'scapy'}
 BOT_NAME_DICT = {1: 'tuling123', 2: 'yigeai', 3: 'qingyunke'}
+# 用于星座的正则
+BIRTHDAY_COMPILE = re.compile(r'\-?(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$')
 
 
 def get_dictum_info(channel):
     """
-    获取每日提醒。
+    获取每日一句。
     :return:str
     """
     if not channel:
@@ -37,8 +46,8 @@ def get_weather_info(cityname):
     """
     if not cityname:
         return
-    return get_today_weather(cityname)
-    # return get_rttodayweather(cityname)
+    # return get_today_weather(cityname)
+    return get_sojson_weather(cityname)
 
 
 def get_bot_info(message, userId=''):
@@ -48,7 +57,8 @@ def get_bot_info(message, userId=''):
     :param message:str, 发送的话
     :return:str, 回复的话
     """
-    channel = get_yaml().get('bot_channel', 3)
+    channel = config.get('bot_channel',3)
+    # channel = get_yaml().get('', 3)
     source = BOT_NAME_DICT.get(channel, 'qingyunke')
     if source:
         addon = importlib.import_module('everyday_wechat.control.bot.' + source, __package__)
@@ -62,7 +72,7 @@ def get_bot_info(message, userId=''):
     return None
 
 
-def get_diff_time(start_date):
+def get_diff_time(start_date, start_msg=''):
     """
     # 在一起，一共多少天了。
     :param start_date:str,日期
@@ -70,44 +80,45 @@ def get_diff_time(start_date):
     """
     if not start_date:
         return None
-    try:
-        start_datetime = datetime.strptime(start_date, '%Y-%m-%d')
-        day_delta = (datetime.now() - start_datetime).days + 1
+    rdate = r'^[12]\d{3}[ \/\-](?:0?[1-9]|1[012])[ \/\-](?:0?[1-9]|[12][0-9]|3[01])$'
+    start_date = start_date.strip()
+    if not re.search(rdate, start_date):
+        print('日期填写出错..')
+        return
+    start_datetime = datetime.strptime(start_date, '%Y-%m-%d')
+    day_delta = (datetime.now() - start_datetime).days + 1
+    if start_msg and start_msg.count('{}') == 1:
+        delta_msg = start_msg.format(day_delta)
+    else:
         delta_msg = '宝贝这是我们在一起的第 {} 天。'.format(day_delta)
-    except Exception as exception:
-        print(exception)
-        delta_msg = None
     return delta_msg
 
 
-def get_xzw_info(birthday_str):
-    """获取今日、明日运势发送文本
-        birthday_str :  "10-12" 或  "1980-01-08"
+def get_constellation_info(birthday_str):
     """
-    if not birthday_str: return
-
-    birthday_list = birthday_str.split("-")
-    try:
-        if len(birthday_list) == 3:
-            month, day = int(birthday_list[1]), int(birthday_list[2])
-        elif len(birthday_list) == 2:
-            month, day = int(birthday_list[0]), int(birthday_list[1])
-    except Exception as e:
-        print('您输入的生日格式有误，请确认！（例："1980-01-08" 或 "01-08"）')
+    获取星座运势
+    :param birthday_str:  "10-12" 或  "1980-01-08" 或 星座名
+    :return:
+    """
+    if not birthday_str:
         return
+    const_name = get_constellation_name(birthday_str)
+    if not const_name:
+        print('星座名填写错误')
+        return
+    return get_today_horoscope(const_name)
 
-    resp = ""
-    constellation = get_constellation(month, day)
-    data_list = get_xzw_data_list(constellation)
 
-    for item in data_list:
-        resp += "\n\n" + item['title_name'] + "（" + item['date'] + "）\n"
-        resp += "幸运颜色：%s \n" % item['lucky_colour']
-        resp += "幸运数字：%s \n" % item['lucky_num']
-        for detail in item['detail_info']:
-            resp += "- " + detail['name'] + ": \n"
-            resp += detail['info'] + "\n"
-    return resp
+def get_calendar_info(alldata=True):
+    """ 获取万年历 """
+    if alldata:
+        date = datetime.now().strftime('%Y-%m-%d')
+        return get_sojson_calendar(date)
+    else:
+        time_now = datetime.now()
+        week = WEEK_DICT[time_now.strftime('%A')]
+        date = time_now.strftime('%Y-%m-%d')
+        return '{} {}'.format(date, week)
 
 
 if __name__ == '__main__':
