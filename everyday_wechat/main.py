9c46ce10abf2fbbea3c6ea8de4d63e86a0a7e5e5
llht <hantongliu@outlook.com>
2019-07-01 13:54:43 +0800

    修改命令行显示二维码部分enableCmdQR数值为1

--- everyday_wechat/main.py
+++ everyday_wechat/main.py
@@ -75,7 +75,7 @@ def is_online(auto_login=False):
             itchat.run(blockThread=True)
         else:
             # 命令行显示登录二维码。
-            itchat.auto_login(enableCmdQR=2, hotReload=hotReload, loginCallback=loginCallback,
+            itchat.auto_login(enableCmdQR=1, hotReload=hotReload, loginCallback=loginCallback,
                               exitCallback=exitCallback)
             itchat.run(blockThread=True)
         if _online():
