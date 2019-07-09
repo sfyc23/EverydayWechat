9875ef985b76cf5f71cd75e8dc91901f68d29b3e
sfyc23 <sfyc23@gmail.com>
2019-07-09 22:29:47 +0800

    add version 0.1.012

---
+++ everyday_wechat/version.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Project: EverydayWechat-Github
+Creator: DoubleThunder
+Create time: 2019-07-07 12:25
+Introduction:
+"""
+script_name = 'EverydayWechat'
+__version__ = '0.1.012'
+
