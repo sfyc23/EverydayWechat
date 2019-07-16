37994d46c80680d6491b5ed50b970964bd194657
sfyc23 <sfyc23@gmail.com>
2019-07-16 12:39:26 +0800

    review code

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ from datetime import datetime
 
 try:
     from everyday_wechat import __version__
+
     print('EverydayWechat 程序版本号：{}'.format(__version__))
     _date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print('当前时间：{}'.format(_date))
@@ -19,13 +20,12 @@ except Exception as exception:
     exit(1)
 
 
-
 def run():
     """ 主程序入口"""
 
     # 判断当前环境是否为 python 3
     if sys.version_info[0] == 2:
-        print('此项目不支持 Python 版本！')
+        print('此项目不支持 Python 2 版本！')
         return
 
     # 检查依赖库是否都已经安装上
@@ -33,19 +33,20 @@ def run():
         import itchat
         import apscheduler
         import requests
-        # import requests_html
-        from bs4 import BeautifulSoup
+        from bs4 import BeautifulSoups
         if itchat.__version__ != '1.3.10':
             print('请将 itchat 的版本升级至 1.3.10！')
             return
 
-    except (ModuleNotFoundError) as error:
-        # No module named 'teim'
-        no_modules = re.findall(r"named '(.*?)'$", str(error))
-        if no_modules:
-            print('当前环境缺少 {} 库'.format(no_modules[0]))
-        else:
-            print('当前环境库不完整')
+    except (ModuleNotFoundError, ImportError) as error:
+        if isinstance(error, ModuleNotFoundError):
+            no_modules = re.findall(r"named '(.*?)'$", str(error))
+            if no_modules:
+                print('当前运行环境缺少 {} 库'.format(no_modules[0]))
+            print(str(error))
+        elif isinstance(error, ImportError):
+            print('当前运行环境引入库出错')
+            print(str(error))
         return
 
     try:
@@ -58,8 +59,7 @@ def run():
         print(str(exception))
         return
 
-    main.run()
-
+    # main.run()
 
 
 if __name__ == '__main__':
